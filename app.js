b63b66845c31abad2188d6052b04d81b34e87fbb
Larissa Verhuelsdonk <larissa.verhuelsdonk@gmail.com>
2018-05-20 23:10:52 +0200

    installs middleware to validate voting options. Adds custom error handler.

--- app.js
+++ app.js
@@ -16,8 +16,18 @@ app.use("/questions", routes);
 app.use((req, res, next) => {
     const err = new Error("Not Found");
     err.status = 404;
-    next();
+    next(err);
 })
+
+// Error Handler
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
